0ae8fe582a339c19c1bbb3a17cf4b67934f78294
Renato Chitolina <renato.chitolina@hotmail.com>
2024-10-25 03:57:52 +0000

    Changing program to the main class style

--- HelloDevContainer/Program.cs
+++ HelloDevContainer/Program.cs
@@ -1,31 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app
+            .MapGet("/hello", () => "Hello I'm running inside a docker image in WSL")
+            .WithName("GetHello")
+            .WithOpenApi();
+
+        app
+            .MapGet("/helloMachine1", () => "Hello I was generated from the machine setup 1")
+            .WithName("GetMachine1")
+            .WithOpenApi();
+
+        app
+            .MapGet("/helloMachine2", () => "Hello I was generated from the machine setup 2")
+            .WithName("GetMachine2")
+            .WithOpenApi();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app
-    .MapGet("/hello", () => "Hello I'm running inside a docker image in WSL")
-    .WithName("GetHello")
-    .WithOpenApi();
-
-app
-    .MapGet("/helloMachine1", () => "Hello I was generated from the machine setup 1")
-    .WithName("GetMachine1")
-    .WithOpenApi();
-
-app
-    .MapGet("/helloMachine2", () => "Hello I was generated from the machine setup 2")
-    .WithName("GetMachine2")
-    .WithOpenApi();
-
-app.Run();
